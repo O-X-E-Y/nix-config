4b91ddfa8719a06e1d875aa51a99a0e6bcc0f1a8
Oxey <lucoerlemans37@gmail.com>
2024-05-28 10:46:44 +0200

    fix etcher electron dep

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -43,6 +43,9 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
+      permittedInsecurePackages = [
+        "electron-19.1.9"
+      ];
     };
   };
 
